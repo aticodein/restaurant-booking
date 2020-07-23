f400d0112a0a1aaff24bfeac23714173ec09667a
Attila Szaloki <szalokiattila@gmail.com>
2020-07-23 10:41:26 +0000

    first model added

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class bookingItem(models.Model):
+    name = models.CharField(max_length=29, null=False, blank=False)
+    time = models.DecimalField(max_digits=4, decimal_places=2,
+                               null=False, blank=False)
+    adultNum = models.DecimalField(max_digits=2, decimal_places=0,
+                                   null=False, blank=False)
+    date = models.DateField(auto_now=False, auto_now_add=False,
+                            null=False, blank=False)
+    childNum = models.DecimalField(max_digits=2, decimal_places=0,
+                                   null=False, blank=False)
+    highChair = models.DecimalField(max_digits=1, decimal_places=0,
+                                    null=False, blank=False)
+    roomNum = models.DecimalField(max_digits=3, decimal_places=0,
+                                  null=False, blank=False)
+    comment = models.CharField(max_length=199, null=False, blank=False)
+    email = models. EmailField(max_length=254, null=False, blank=True)
